ebad135e86342ac7e29dd4bbc5856ed70a558514
root <root@DESKTOP-159IP4Q>
2023-09-17 12:54:08 +0800

    Flash Messages | Laravel Livewire 3 Course for Beginners EP5

--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,4 +1,8 @@
 <div>
+	@if (session('success'))
+		<span class="px-3 py-3 bg-green-600 text-white rounded">{{ session('success') }}</span>
+	@endif
+
 	<form class="p-5" wire:submit="createNewUser">
 		<input class="block rounded border border-gray-100 px-3" wire:model="name" type="text" placeholder="name">
 		@error ('name')
