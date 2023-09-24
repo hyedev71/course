f62ea79e0fda11f8a2a3bcef6d779869860ba462
root <root@DESKTOP-159IP4Q>
2023-09-25 01:30:22 +0800

    Polling, Refresh Components automatically | Laravel Livewire 3 for Beginners EP10

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -50,8 +50,8 @@
 			</button>
 
 			<button wire:loading.attr="disabled" wire:loading.class="bg-blue-100 text-gray-100" wire:loading.class.remove="hover:bg-blue-800" type="submit" class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline relative">
-				<span class="mr-2">Create +</span>
-				<svg wire:loading class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+				<span class="">Create +</span>
+				<svg wire:loading class="animate-spin h-5 w-5 ml-3" viewBox="0 0 24 24">
 					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
 					<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.96 7.96 0 014 12H0c0 6.627 5.373 12 12 12v-4a7.963 7.963 0 01-6-2.709z"></path>
 				</svg> 
