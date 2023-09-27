ae395c77da7486a1b1c8bcf1027eae65375d1882
root <root@DESKTOP-159IP4Q>
2023-09-28 00:03:50 +0800

    Pass Data to Components & Mount | Laravel Livewire 3 for Beginners EP13

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -1,4 +1,49 @@
 
+<div class="mt-5 p-5 mx-auto w-2/5 shadow border-teal-500 border-t-2">
+
+	<div class="w-1/4 animate-pulse flex relative mb-5">
+		<div class="h-5 bg-gray-500 dark:bg-gray-700 mt-3 mb-3 rounded w-3/4 mr-3"></div>
+		<div class="h-5 bg-gray-400 dark:bg-gray-700 mt-3 mb-3 rounded w-1/4"></div>
+	</div>
+
+	<div class="w-full animate-pulse">
+		<div class="h-10 bg-gray-400 dark:bg-gray-600 mt-3 mb-3 rounded"></div>
+	</div>
+
+	<div role="status" class="max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+		<div class="flex items-center justify-between">
+			<div>
+				<div class="h-6 bg-gray-500 rounded-full dark:bg-gray-800 w-24 mb-3"></div>
+				<div class="w-32 h-5 bg-gray-400 rounded-full dark:bg-gray-700"></div>
+			</div>
+			<div class="h-6 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+		</div>
+		<div class="flex items-center justify-between pt-4">
+			<div>
+				<div class="h-6 bg-gray-500 rounded-full dark:bg-gray-800 w-24 mb-3"></div>
+				<div class="w-32 h-5 bg-gray-400 rounded-full dark:bg-gray-700"></div>
+			</div>
+			<div class="h-6 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+		</div>
+		<div class="flex items-center justify-between pt-4">
+			<div>
+				<div class="h-6 bg-gray-500 rounded-full dark:bg-gray-800 w-24 mb-3"></div>
+				<div class="w-32 h-5 bg-gray-400 rounded-full dark:bg-gray-700"></div>
+			</div>
+			<div class="h-6 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+		</div>
+		<span class="sr-only text-2xl">Loading...</span>
+	</div>
+
+	<div class="w-full mt-4 animate-pulse">
+		<div class="h-20 bg-gray-400 dark:bg-gray-700 mt-3 mb-3 rounded"></div>
+	</div>
+
+
+</div>
+
+
+{{--
 <div class="mt-10 p-5 mx-auto w-5/6 shadow border-teal-500 border-t-2">
 	<div class="animate-pulse">
 		<div class="h-4 bg-gray-200 mt-3 mb-6 rounded"></div>
@@ -8,3 +53,4 @@
 		<div class="h-4 bg-gray-200 mb-6 rounded"></div>
 	</div>
 </div>
+--}}
